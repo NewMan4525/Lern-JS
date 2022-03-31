37d5d72c22efccc7527c92a0b6f43046250942bd
Andrey L <newman4525@gmail.com>
2022-03-31 06:01:04 +0300

    lesson02 is complite

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("Hello js")
+let title = 'project X';
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 100;
+let rollback = 10;
+let fullPrice = 100000;
+let adaptive = true;
+
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+
+console.log(screens.length);
+
+console.log("Стоймость верстки экранов",screenPrice,"долларов" );
+console.log("Стоимость разработки сайта", `${fullPrice}`, "юани" );
+console.log(screens.toLowerCase().split(", "))
+console.log(fullPrice * (rollback/100))
